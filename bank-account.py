4a9bc12a4986d18d98c71d02e16f0973357bcd3d
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-10-21 11:22:58 -0700

    Update bank-account.py

--- bank-account.py
+++ bank-account.py
@@ -1,31 +1,35 @@
 class BankAccount:
-  def __init__(self, full_name: str, account_number: int, balance = 0) -> None:
+  def __init__(self, full_name: str, account_number: float, amount = 0):
     self.name = full_name
     self.number = account_number
-    self.amount = balance
-  def desposit(self, amount: float) -> None:
-    self.amount += amount
+    self.balance = amount
+  def desposit(self):
+    amount = float(input("Enter deposit amount: "))
+    self.balance += amount
     print(f"Amount Deposited: ${amount} New Balance: ${self.balance}")
-  def withdraw(self, amount: float) -> None:
-    if amount > self.amount:
+  def withdraw(self):
+    amount = float
+    if amount > self.balance:
       print("Insufficicent Funds")
-      self.amount -= 10
+      self.balance -= 10
     else:
-      self.amount -= amount
-      print(f"Amount Withdrawn: ${amount} New Balance: ${self.amount}")
+      self.balance -= amount
+      print(f"Amount Withdrawn: ${amount} New Balance: ${self.balance}")
   def get_balance(self) -> float:
-    print(f"Current Balance of Account: ${self.amount}")
-    return self.amount
-  def add_interest(self) -> None:
-    interest = self.amount * 0.0083
-    self.amount +- interest
-  def print_statement(self) -> None:
-    return str(f"{self.name}\nAccount Number: ****{str(self.number)[-4:]}\nBalance: ${self.amount}")
+    print(f"Current Balance of Account: ${self.balance}")
+    return self.balance
+  def add_interest(self):
+    interest = self.balance * 0.0083
+    self.balance +- interest
+  def print_statement(self):
+    return str(f"{self.name}\nAccount Number: ****{str(self.number)[-4:]}\nBalance: ${self.balance}")
 
 PenAccount = BankAccount("Pen", "1471521", "10.93")
 EraserAccount = BankAccount("Eraser", "5326165", "11.51")
 BlockyAccount = BankAccount("Blocky", "8381362", "14.20")
+MitchellAccount = BankAccount("Mitchell", "3141592", "400,000.00")
 
 print(PenAccount.print_statement())
 print(EraserAccount.print_statement())
 print(BlockyAccount.print_statement())
+print(MitchellAccount.desposit())
