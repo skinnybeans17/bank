f331b6edabcd71d2e28e5e930850cf34af92f9e9
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-10-21 18:18:34 -0700

    Update bank-account.py

--- bank-account.py
+++ bank-account.py
@@ -1,17 +1,14 @@
 class BankAccount:
-  def __init__(self, full_name: str, account_number: float, amount = 0):
+  def __init__(self, full_name, account_number: float, amount: 0):
     self.name = full_name
     self.number = account_number
     self.balance = amount
-  def desposit(self):
-    amount = float(input("Enter deposit amount: "))
+  def desposit(self, amount):
     self.balance += amount
-    print(f"Amount Deposited: ${amount} New Balance: ${self.balance}")
-  def withdraw(self):
-    amount = float
+    print(f'Amount Deposited: ${amount} New Balance: ${self.balance}\n')
+  def withdraw(self, amount):
     if amount > self.balance:
       print("Insufficicent Funds")
-      self.balance -= 10
     else:
       self.balance -= amount
       print(f"Amount Withdrawn: ${amount} New Balance: ${self.balance}")
@@ -24,12 +21,36 @@ class BankAccount:
   def print_statement(self):
     return str(f"{self.name}\nAccount Number: ****{str(self.number)[-4:]}\nBalance: ${self.balance}")
 
-PenAccount = BankAccount("Pen", "1471521", "10.93")
-EraserAccount = BankAccount("Eraser", "5326165", "11.51")
-BlockyAccount = BankAccount("Blocky", "8381362", "14.20")
-MitchellAccount = BankAccount("Mitchell", "3141592", "400,000.00")
-
+PenAccount = BankAccount("Pen", 1471521, 1093)
+PenAccount.desposit(2426)
 print(PenAccount.print_statement())
+PenAccount.add_interest()
+print(PenAccount.print_statement())
+PenAccount.withdraw(285)
+print(PenAccount.print_statement())
+
+EraserAccount = BankAccount("Eraser", 5326165, 1151)
+EraserAccount.desposit(209)
 print(EraserAccount.print_statement())
+EraserAccount.add_interest()
+print(EraserAccount.print_statement())
+PenAccount.withdraw(185)
+print(PenAccount.print_statement())
+
+BlockyAccount = BankAccount("Blocky", 8381362, 1420)
+BlockyAccount.desposit(11229)
 print(BlockyAccount.print_statement())
-print(MitchellAccount.desposit())
+BlockyAccount.add_interest()
+print(BlockyAccount.print_statement())
+BlockyAccount.withdraw(239)
+print(BlockyAccount.print_statement())
+
+MitchellAccount = BankAccount("Mitchell", 3141592, 0)
+MitchellAccount.desposit(400000)
+print(MitchellAccount.print_statement())
+MitchellAccount.add_interest()
+print(MitchellAccount.print_statement())
+MitchellAccount.withdraw(150)
+print(MitchellAccount.print_statement())
+
+  
