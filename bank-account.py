812894d9fc4d459318de749ac56aaaca32ba49b6
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-10-22 15:32:11 -0700

    Update bank-account.py

--- bank-account.py
+++ bank-account.py
@@ -1,25 +1,25 @@
 class BankAccount:
-  def __init__(self, full_name, account_number: float, amount: 0):
+  def __init__(self, full_name, account_number: float, amount: int):
     self.name = full_name
     self.number = account_number
     self.balance = amount
   def desposit(self, amount):
     self.balance += amount
-    print(f"Amount Deposited: ${amount} New Balance: ${self.balance}")
+    print(f"Amount Deposited: ${amount} New Balance: ${round(self.balance, 2)}")
   def withdraw(self, amount):
     if amount > self.balance:
       print("Insufficicent Funds")
     else:
       self.balance -= amount
-      print(f"Amount Withdrawn: ${amount} New Balance: ${self.balance}")
+      print(f"Amount Withdrawn: ${amount} New Balance: ${round(self.balance, 2)}")
   def get_balance(self) -> float:
     print(f"Current Balance of Account: ${self.balance}")
     return self.balance
   def add_interest(self):
     interest = self.balance * 0.00083
-    self.balance +- interest
+    self.balance += interest
   def print_statement(self):
-    return str(f"{self.name}\nAccount Number: ****{str(self.number)[-4:]}\nBalance: ${self.balance}")
+    return str(f"{self.name}\nAccount Number: ****{str(self.number)[-4:]}\nBalance: ${round(self.balance, 2)}")
 
 
 PenAccount = BankAccount("Pen", "14715215", 1093)
@@ -53,3 +53,4 @@ MitchellAccount.add_interest()
 print(MitchellAccount.print_statement())
 MitchellAccount.withdraw(150)
 print(MitchellAccount.print_statement())
+
